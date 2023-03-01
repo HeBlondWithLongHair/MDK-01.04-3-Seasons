37abf8a36dcbf921c2543a31f3311d5778d6f526
HeBlondWithLongHair <advol231231231@gmail.com>
2023-03-01 19:24:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programm
+{
+    public static void Main(string[] args)
+    {
+        SeasonInfo winter = new SeasonInfo(Seasons.Winter, -20);
+        SeasonInfo spring = new SeasonInfo(Seasons.Spring, +5);
+        SeasonInfo summer = new SeasonInfo(Seasons.Summer, +25);
+        SeasonInfo autumn = new SeasonInfo (Seasons.Autumn, 0);
+
+        winter.PrintInfo();
+        spring.PrintInfo();
+        summer.PrintInfo();
+        autumn.PrintInfo();
+
+        Console.WriteLine();
+
+        foreach (Seasons season in Enum.GetValues(typeof(Seasons)))
+        {
+            SeasonInfo.PrintSeasondDescription(season);
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
---
+++ SeasonInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class SeasonInfo
+{
+    int temperature;
+    Seasons season;
+
+    public SeasonInfo(Seasons season, int temperature)
+    {
+        this.season = season;
+        this.temperature = temperature;
+    }
+
+    public string getDescription()
+    {
+        if (season == Seasons.Summer)
+        {
+            return "Жаркое время года";
+        }
+        if (season == Seasons.Winter)
+        {
+            return "Холодное время года";
+        }
+        if (season == Seasons.Spring)
+        {
+            return "Теплое время года";
+        }
+        if (season == Seasons.Autumn)
+        {
+            return "Прохладное время года";
+        }
+        else
+        {
+            return "Нет такого время года";
+        }
+
+        
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Время года: {season}\nСредняя температура: {temperature} градусов\nОписание: {getDescription()}");
+    }
+
+    public static void PrintSeasondDescription(Seasons season)
+    {
+        switch (season)
+        {
+            case Seasons.Winter:
+                Console.WriteLine("Зима - это время года, когда все покрыто снегом и очень холодно");
+                break;
+
+            case Seasons.Spring:
+                Console.WriteLine("Весна - это время года, когда все начинает цвести и становится теплее");
+                break;
+
+            case Seasons.Summer:
+                Console.WriteLine("Лето - мое любимое время года, тепло и красиво");
+                break;
+
+            case Seasons.Autumn:
+                Console.WriteLine("Осень - это время года, когда опадают листь, становится холоднее");
+                break;
+
+        }    
+    }
+   
+
+
+
+}
+
+
+
+
+
+
